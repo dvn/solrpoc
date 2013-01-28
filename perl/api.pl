719315d22759c3cc06b1189d2d612465f1d73080
Philip Durbin <philip_durbin@harvard.edu>
2013-01-28 09:38:26 -0500

    grab some author info

--- perl/api.pl
+++ perl/api.pl
@@ -9,7 +9,8 @@ use Data::Dumper;
 binmode STDOUT, ':encoding(utf8)';
 
 Readonly my $api_url => 'https://dvn.iq.harvard.edu/dvn/api';
-Readonly my $titlesearch_url => "$api_url/metadataSearch/title:quantitative";
+# https://dvn.iq.harvard.edu/dvn/api/metadataSearchFields
+Readonly my $titlesearch_url => "$api_url/metadataSearch/authorName:King";
 
 my $titlesearch_xml = get($titlesearch_url);
 my $titlesearch_dd  = XMLin($titlesearch_xml);
@@ -17,23 +18,69 @@ my $titlesearch_dd  = XMLin($titlesearch_xml);
 my $doc_add_xml = "<add>\n";
 my $hashref;
 
+my $count;
 while ( my $study = shift @{ $titlesearch_dd->{searchHits}{study} } ) {
     next if $study->{ID} eq 'hdl:1902.1/15263';
     next if $study->{ID} eq 'hdl:1902.1/16573';
+
+    next if $study->{ID} eq 'hdl:1902.1/01996';
+    next if $study->{ID} eq 'hdl:1902.1/01964';
+    next if $study->{ID} eq 'hdl:1902.1/01978';
+    next if $study->{ID} eq 'hdl:1902.1/02012';
+    next if $study->{ID} eq 'hdl:1902.1/01967';
+    next if $study->{ID} eq 'hdl:1902.1/01977';
+    next if $study->{ID} eq 'hdl:1902.1/01979';
+    next if $study->{ID} eq 'hdl:1902.1/01970';
+    next if $study->{ID} eq 'hdl:1902.1/01989';
+    next if $study->{ID} eq 'hdl:1902.1/01980';
+    next if $study->{ID} eq 'hdl:1902.1/01993';
+    next if $study->{ID} eq 'hdl:1902.1/01960';
+    next if $study->{ID} eq 'hdl:1902.1/01992';
+    next if $study->{ID} eq 'hdl:1902.1/01994';
+    next if $study->{ID} eq 'hdl:1902.1/01963';
+    next if $study->{ID} eq 'hdl:1902.1/01966';
+    next if $study->{ID} eq 'hdl:1902.1/01976';
+    next if $study->{ID} eq 'hdl:1902.1/02008';
+    next if $study->{ID} eq 'hdl:1902.1/02009';
+    next if $study->{ID} eq 'hdl:1902.1/01969';
+    next if $study->{ID} eq 'hdl:1902.1/01997';
+    next if $study->{ID} eq 'hdl:1902.1/01972';
+    next if $study->{ID} eq 'hdl:1902.1/02010';
+    next if $study->{ID} eq 'hdl:1902.1/01956';
+    next if $study->{ID} eq 'hdl:1902.1/02007';
+    next if $study->{ID} eq 'hdl:1902.1/01998';
+    next if $study->{ID} eq 'hdl:1902.1/01962';
+    next if $study->{ID} eq 'hdl:1902.1/01975';
+    last if $study->{ID} eq 'hdl:1902.1/01995';
+    #last if $study->{ID} eq 'hdl:1902.1/01989';
+    #say $study->{ID};
     #https://dvn.iq.harvard.edu/dvn/api/metadata/hdl:1902.1/SZKONDGOMF
     my $metadata_url = "https://dvn.iq.harvard.edu/dvn/api/metadata/$study->{ID}";
     my $metadata_xml = get($metadata_url);
-    my $metadata_dd = XMLin($metadata_xml);
+    my $metadata_dd  = XMLin($metadata_xml);
+    #print Dumper $metadata_dd;
     my $abstract = $metadata_dd->{stdyDscr}{stdyInfo}{abstract};
-    my $title = $metadata_dd->{stdyDscr}{citation}{titlStmt}{titl};
-    my $date = $metadata_dd->{stdyDscr}{citation}{distStmt}{distDate}{date};
+    my $title    = $metadata_dd->{stdyDscr}{citation}{titlStmt}{titl};
+    #my $date = $metadata_dd->{stdyDscr}{citation}{distStmt}{distDate}{date};
+    my $date = $metadata_dd->{stdyDscr}{citation}{prodStmt}{prodDate}{date};
+    #say $study->{ID};
+    next if ref $metadata_dd->{stdyDscr}{citation}{rspStmt}{AuthEnty} ne 'ARRAY';
+    #say ref $metadata_dd->{stdyDscr}{citation}{rspStmt}{AuthEnty}[0];
+    next if ref $metadata_dd->{stdyDscr}{citation}{rspStmt}{AuthEnty}[0] ne 'HASH';
+    #print Dumper $metadata_dd->{stdyDscr}{citation}{rspStmt}{AuthEnty};
+    #say $count++;
+    #my $author;
+    my $author = $metadata_dd->{stdyDscr}{citation}{rspStmt}{AuthEnty}[0]{content};
+    #next;
+    #say $author;
     $date = $date ? $date : 'N/A';
     ($date) = $date =~ /^(\d\d\d\d)/;
     my $hashref = {
         field => [
-            { name => 'id',    content => $study->{ID} },
-            { name => 'title', content => $title },
-            { name => 'cat',   content => $date },
+            { name => 'id',     content => $study->{ID} },
+            { name => 'title',  content => $title },
+            { name => 'cat',    content => $date },
+            { name => 'author', content => $author },
         ]
     };
     my $doc_xml = XMLout( $hashref, RootName => 'doc' );
