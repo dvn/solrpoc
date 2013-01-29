de614cb5902b1504b8385d435425b706f84a5a34
Philip Durbin <philip_durbin@harvard.edu>
2013-01-29 11:27:51 -0500

    use exec-maven-plugin and `mvn exec:java`

--- solr-search/go.sh
+++ solr-search/go.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-mvn clean && mvn package && java -cp target/solr-search-1.0-SNAPSHOT.jar edu.harvard.iq.pdurbin.solrjsearcher.App
+#mvn clean && mvn package && java -cp target/solr-search-1.0-SNAPSHOT.jar edu.harvard.iq.pdurbin.solrjsearcher.App
+mvn clean compile exec:java
